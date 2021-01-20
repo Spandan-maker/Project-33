9ea07142b889b7273c985983c93369c9d0eda058
Spandan <spandanmundhra123@gmail.com>
2021-01-20 21:05:25 +0530

    updated

---
+++ Desktop/Project-33/PRO-C33-master/sketch.js
@@ -0,0 +1,103 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var divisionHeight=300;
+
+var score = 0;
+var count = 0;
+
+//var PLAY = "PLAY";
+//var END = "END";
+var gameState = "PLAY";
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  
+  textSize(20)
+  text("Score : "+score,20,30);
+
+  Engine.update(engine);
+  
+  ground.display();
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%100===0){
+     particles.push(new Particle(random(mouseX, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+}
+
+function mousePressed(){
+  if(gameState !== "END"){
+    particle = new Particle(mouseX, 10, 10, 10);
+    count++;
+  }
+}
+
+function Count(){
+  if(count >= 5){
+    gamestate = "END";
+  }
+}
