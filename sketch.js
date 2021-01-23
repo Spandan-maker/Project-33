80dea48ff7743bc4af6d14fe8b6e622632f89ae0
Spandan <74659727+Spandan-maker@users.noreply.github.com>
2021-01-23 18:13:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,16 @@
 var Engine = Matter.Engine,
-  World = Matter.World,
-  Events = Matter.Events,
-  Bodies = Matter.Bodies;
- 
+    World = Matter.World,
+    Events = Matter.Events,
+    Bodies = Matter.Bodies; 
 var particles = [];
 var plinkos = [];
-var divisions = [];
+var divisions =[];
+var particle;
 
 var divisionHeight=300;
-
-var score = 0;
+var score =0;
 var count = 0;
-
-//var PLAY = "PLAY";
-//var END = "END";
-var gameState = "PLAY";
+var gameState ="start";
 
 function setup() {
   createCanvas(800, 800);
@@ -22,82 +18,104 @@ function setup() {
   world = engine.world;
   ground = new Ground(width/2,height,width,20);
 
-
    for (var k = 0; k <=width; k = k + 80) {
      divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
    }
-
-
-    for (var j = 75; j <=width; j=j+50) 
-    {
-    
+    for (var j = 75; j <=width; j=j+50) {
        plinkos.push(new Plinko(j,75));
     }
 
-    for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,175));
+    for (var j = 50; j <=width-10; j=j+50) {
+        plinkos.push(new Plinko(j,175));
     }
 
-     for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,275));
+    for (var j = 75; j <=width; j=j+50) {
+        plinkos.push(new Plinko(j,275));
     }
 
-     for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,375));
+    for (var j = 50; j <=width-10; j=j+50) {
+        plinkos.push(new Plinko(j,375));
     }
-
-    
-
     
 }
  
-
-
 function draw() {
   background("black");
-  
-  textSize(20)
-  text("Score : "+score,20,30);
-
+  textSize(35)
+  text("Score : "+score,20,40);
+  fill("white");
+  //text(mouseX + "," + mouseY, 20, 50);
+  textSize(35)
+  text(" 500 ", 5, 550);
+  text(" 500 ", 80, 550);
+  text(" 500 ", 160, 550);
+  text(" 500 ", 240, 550);
+  text(" 100 ", 320, 550);
+  text(" 100 ", 400, 550);
+  text(" 100 ", 480, 550);
+  text(" 200 ", 560, 550);
+  text(" 200 ", 640, 550);
+  text(" 200 ", 720, 550);
   Engine.update(engine);
-  
   ground.display();
   
-   for (var i = 0; i < plinkos.length; i++) {
-     
-     plinkos[i].display();
-     
-   }
-   if(frameCount%100===0){
-     particles.push(new Particle(random(mouseX, width/2+30), 10,10));
-     score++;
-   }
+  if ( gameState =="end") {
+    
+    textSize(100);
+    text("GameOver", 150, 250);
+    //return
+  }
+
+  for (var i = 0; i < plinkos.length; i++) {
+     plinkos[i].display();  
+  }
  
-  for (var j = 0; j < particles.length; j++) {
-   
-     particles[j].display();
-   }
-   for (var k = 0; k < divisions.length; k++) {
-     
+    if(particle!=null)
+    {
+       particle.display();
+        
+        if (particle.body.position.y>760)
+        {
+              if (particle.body.position.x < 300) 
+              {
+                  score=score+500;      
+                  particle=null;
+                  if ( count>= 5) gameState ="end";                          
+              }
+
+
+              else if (particle.body.position.x < 600 && particle.body.position.x > 301 ) 
+              {
+                    score = score + 100;
+                    particle=null;
+                    if ( count>= 5) gameState ="end";
+
+              }
+              else if (particle.body.position.x < 900 && particle.body.position.x > 601 )
+              {
+                    score = score + 200;
+                    particle=null;
+                    if ( count>= 5)  gameState ="end";
+
+              }      
+              
+        }
+  
+      }
+
+   for (var k = 0; k < divisions.length; k++) 
+   {
      divisions[k].display();
    }
+ 
 }
 
-function mousePressed(){
-  if(gameState !== "END"){
-    particle = new Particle(mouseX, 10, 10, 10);
-    count++;
-  }
-}
 
-function Count(){
-  if(count >= 5){
-    gamestate = "END";
-  }
+function mousePressed()
+{
+  if(gameState!=="end")
+  {
+      count++;
+     particle=new Particle(mouseX, 10, 10, 10); 
+  }   
 }
